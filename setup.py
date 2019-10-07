24f7aeb45a9547aa7c92c5921ae7fba4e0e9df46
Steven Miers <smiers@glgroup.com>
2019-10-07 12:28:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,22 +11,26 @@ from setuptools import setup
 
 DESCRIPTION = "auto-cli-py: python package to automatically create CLI commands from function via introspection"
 
+long_description = []
 with open("README.md", "r") as fh:
-  long_description = fh.read()
+  long_description.append(fh.read())
 
-long_description += "\n---\n## Example\n"
+long_description.append("---")
+long_description.append("---## Example")
+long_description.append("```python")
 with open("examples.py", "r") as fh:
-  long_description += fh.read()
+  long_description.append(fh.read())
+long_description += "```"
 
 setup(
   name='auto-cli-py',
-  version='0.4.5',
+  version='0.4.6',
   description=DESCRIPTION,
   url='http://github.com/tangledpath/auto-cli-py',
   author='Steven Miers',
   author_email='steven.miers@gmail.com',
   include_package_data=True,
-  long_description=long_description,
+  long_description="\n".join(long_description),
   long_description_content_type="text/markdown",
   packages=['auto_cli'],
   python_requires='>=3.6',
